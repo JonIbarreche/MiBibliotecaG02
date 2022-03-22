d3b51deebec3c679c2beff7172c3b24fc93dcf29
JonIbarreche <55731819+JonIbarreche@users.noreply.github.com>
2022-03-22 19:18:33 +0100

    usuario.c y usuario.h

---
+++ clases/usuario.c
@@ -0,0 +1,8 @@
+/*
+ * usuario.c
+ *
+ *  Created on: 22 mar. 2022
+ *      Author: jonib
+ */
+
+
--- main.c
+++ main.c
@@ -6,6 +6,9 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "clases/usuario.h"
 
 typedef struct{
 	int id;
