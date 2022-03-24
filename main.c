55cad31d6fe87c8c44033ee09e5f4be0edccb10e
Unai <Unai@192.168.0.102>
2022-03-24 14:05:40 +0100

    Main Prueba

--- main.c
+++ main.c
@@ -1,24 +1,130 @@
-/*
- * main.c
- *
- *  Created on: 22 mar. 2022
- *      Author: jonib
- */
-
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include "Menus/menu.h"
+#include "socio.h"
+#include "usuario.h"
 
-#include "clases/usuario.h"
+int main(void)
+{
+/*
+	Usuario usuarios [100];
 
-typedef struct{
-	int id;
-	float nPaginas;
-	int p;
-} Libro;
+		int posicion = 0;
+		int acceso;
 
-int main (void){
+		int flag = 0;
 
-	return 0;
+		while (flag == 0){
+			int opcion0 = gestionBibliotecasBilbao();
+			//MENU GESTION (LOGIN/REGISTRO/SALIR)
+			switch (opcion0){
+
+			case 1:
+				acceso = inicioSesion(usuarios);
+
+				if (acceso == 1){
+					//flag = 1;
+
+					printf("==============================================================\n");
+					printf("-----------------CUENTA INICIADA CON EXITO--------------------\n");
+
+					int flag2 = 0;
+					//MENU PRINCIPAL (GESTIONAR BIBLIO/IMPORTAR DATOS/BORRAR BD/SALIR)
+					while (flag2 == 0){
+
+						int opcion1 = menuPrincipal();
+
+						switch (opcion1){
+
+						case 1:
+
+							//MENU GESTION DE BIBLIOTECAS
+							while (flag2 == 0){
+
+								int opcion2 = gestionBibliotecas();
+
+								switch (opcion2){
+
+								case 1:
+
+									break;
+
+								case 2:
+
+									break;
+
+								case 3:
+
+									break;
+
+								case 4:
+
+									break;
+
+								case 5:
+
+									break;
+
+								case 6:
+
+									break;
+
+								case 7:
+			 						printf("\n¿Desea volver a la ventana anterior?\n [s/n]");
+									fflush(stdout);
+									scanf("%s", &opcion1);
+									flag2 = 1;
+									break;
+
+								}
+							}
+
+							break;
+
+						case 2:
+
+							break;
+
+						case 3:
+
+							break;
+
+						case 4:
+							printf("\n¿Desea volver a la ventana anterior?\n 1. SI\n 2. NO\n");
+							fflush(stdout);
+							scanf("%s", &opcion0);
+							flag2 = 1;
+							break;
+
+						default:
+							printf("\nCaracter introducido erroneo");
+							break;
+						}
+				}
+				}else{
+					printf("\nAcceso denegado, porfavor intentelo de nuevo\n\n");
+				}
+
+				break;
+
+			case 2:
+				registrarUsuario(usuarios, posicion);
+				posicion = posicion + 1;
+				break;
+
+			case 3:
+				printf("====================================================================\n");
+				printf("----------------------PROGRAMA FINALIZADO---------------------------\n");
+				flag = 1;
+
+				break;
+
+			default:
+				printf("Codigo introducido erroneo\n");
+			}
+
+		}
+		return 0;
+*/
 }
 
-// prueba
