bc5dec620b9f6eaa367b42ec87d51d173e99f573
Unai <Unai@192.168.0.102>
2022-03-24 16:59:29 +0100

    Clases del proyecto

---
+++ biblioteca.h
@@ -0,0 +1,23 @@
+#ifndef BIBLIOTECA_H_
+#define BIBLIOTECA_H_
+
+/* typedef struct{
+	int idLibro;
+	char titulo;
+	char genero;
+	char autor;
+}Libro;
+*/
+
+typedef struct{
+	int idBiblioteca;
+	char nombre;
+	int aforo;
+	char estado;
+	char genero;
+	char instalacion;
+	char barrio;
+//	Libro libro;
+}Biblioteca;
+
+#endif /* BIBLIOTECA_H_ */
---
+++ usuario.h
@@ -0,0 +1,19 @@
+#ifndef USUARIO_H_
+#define USUARIO_H_
+
+typedef struct{
+	char nombre[20];
+	char apellido[20];
+	char nomUsuario[20];
+	char contrasenya[20];
+}Usuario;
+
+//INICIO SESION//
+void imprimirUsuario (Usuario*usuarios, int tamanyo);
+int comprobarUsuario (char name [10], char pass [10], Usuario* usuarios, int tamanyo);
+int inicioSesion(Usuario* usuarios);
+
+//REGISTRARSE//
+void registrarUsuario (Usuario* usuarios, int posicion);
+
+#endif /* USUARIO_H_ */
